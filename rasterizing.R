d237ef04e2b2f221794a3a9e0e5786ada4332df7
Raymond Owino <rowino@arizona.edu>
2026-07-07 09:36:25 +0300

    Removed clipping code to just maintain rasterization

--- rasterizing.R
+++ rasterizing.R
@@ -22,29 +22,3 @@ plot(rivers_utm, add = TRUE, col = "blue")
 # Save the final GeoTIFF to your working directory
 writeRaster(distance_raster, "data/rasters/distance_rivers.tif", overwrite = TRUE)
 
-# clip
-library(terra)
-
-library(terra)
-
-# 1. Load the rasters
-prev_river <- rast("data/rasters/rivers.tif")
-current_river <- rast("data/rasters/distance_rivers.tif")
-
-# 2. Match CRS if they differ
-if (crs(current_river) != crs(prev_river)) {
-  current_river <- project(current_river, crs(prev_river))
-}
-
-# 3. Apply the 200m buffer filter first (0.2 km)
-current_river[current_river > 0.2] <- NA
-
-# 4. CROP: Trim the outer bounding box to match prev_river
-cropped_river <- crop(current_river, ext(prev_river))
-
-# 5. MASK: Force it to take the exact irregular pixel shape of prev_river
-# Any pixel that is NA in prev_river will now become NA in your final output
-final_shaped_river <- mask(cropped_river, prev_river)
-
-# 6. Save the perfectly shaped raster
-writeRaster(final_shaped_river, "data/rasters/current_river_final_shape.tif", overwrite = TRUE)
